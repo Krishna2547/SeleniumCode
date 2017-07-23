a5c962802faa7bababf79c6cbee9dd58378ef094
Teja Krishna <k.t.krishna2547@gmail.com>
2017-07-23 12:57:57 +0530

    initial commit

---
+++ com.krishna.ci/src/test/java/com/krishna/ci/DemoCode.java
@@ -0,0 +1,10 @@
+package com.krishna.ci;
+
+import org.testng.annotations.Test;
+
+public class DemoCode {
+	@Test
+	public void demoMethod(){
+		System.out.println("Integrating Eclipse - Selenium - Maven - GIT - Jenkins");
+	}
+}
