a4d474e59f61c8512f6dc25151866522f2f00d7a
Teja Krishna <k.t.krishna2547@gmail.com>
2017-07-23 13:02:15 +0530

    git integration done

--- com.krishna.ci/src/test/java/com/krishna/ci/DemoCode.java
+++ com.krishna.ci/src/test/java/com/krishna/ci/DemoCode.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class DemoCode {
 	@Test
 	public void demoMethod(){
-		System.out.println("Integrating Eclipse - Selenium - Maven - GIT - Jenkins");
+		
+		System.out.println("Integrated Eclipse - Selenium - Maven - GIT");
 	}
 }
